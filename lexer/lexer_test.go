0e1f371cbc45bd1a61091bd49881b19c7e1cf51e
mp40 <philps.matthew@gmail.com>
2023-05-28 12:28:00 +0200

    extend operators

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,15 +6,17 @@ import (
 	"github.com/mp40/monkey/token"
 )
 
-func TestNextToken2(t *testing.T) {
+func TestNextToken(t *testing.T) {
 	input := `let five = 5;
 	let ten = 10;
-	
+
 	let add = fn(x, y) {
-		x + y;
-	};
-	
+	  x + y;
+ 	};
+
 	let result = add(five, ten);
+	!-/*5;
+	5 < 10 > 5;
 	`
 	tests := []struct {
 		expectedType    token.TokenType
@@ -55,6 +57,18 @@ func TestNextToken2(t *testing.T) {
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
